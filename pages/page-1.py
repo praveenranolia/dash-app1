134dd448c2888b52dc05b68add3f9c7f02617112
praveenranolia <pranveen@ph.iitr.ac.in>
2025-01-20 15:50:12 +0530

    newfilesadded

--- pages/page-1.py
+++ pages/page-1.py
@@ -8,15 +8,16 @@ import plotly.graph_objects as go
 
 dash.register_page(__name__, path="/page-1")
 # importing the data
-df=pd.read_excel('https://docs.google.com/spreadsheets/d/1fJ_8PMI9BLrZaLinmL9pwWxL-yLiixXZ/export?format=xlsx')
+# df=pd.read_excel('https://docs.google.com/spreadsheets/d/1fJ_8PMI9BLrZaLinmL9pwWxL-yLiixXZ/export?format=xlsx')
+
 # )
 # df=pd.read_excel('/Users/praveen/Desktop/APO_DASH/Data_dash_recovery.xlsx')
-# df=pd.read_csv('/Users/praveen/Desktop/APO_DASH/Data_dash_recovery.csv')
+df=pd.read_excel('/Users/praveen/Desktop/APO_DASH/RECOVERY.xlsx')
+# df=pd.read_csv('/Users/praveen/Desktop/APO_DASH/RECOVERY.csv')
 # filling the NaN value to zero just for instance
 df=df.fillna(0)
 # layout of the page
 # card 1 'Recovery'
-
 card_recovery = dbc.Card(
     dbc.CardBody([
         html.H3('RECOVERY'),
@@ -30,23 +31,21 @@ card_quantity = dbc.Card(
     dbc.CardBody([
         html.H3('QUANTITY'),
         html.P('Dispatched: ', style={"fontWeight": "bold"}),
-        html.Div(id='dispatched', children='0', style={"marginBottom": "10px"}),
+        html.Div(id='dispatched', children='0', style={"marginBottom": "10px",'font-size':'40px'}),
         html.P('In Stocks: ', style={"fontWeight": "bold"}),
-        html.Div(id='in_stocks', children='0'),
+        html.Div(id='in_stocks', children='0',style={'font-size':'40px'}),
     ]),
     style={"width": "100%",'height':'100%'}
 )
 grid=dag.AgGrid(
     id="table",
     rowData=df.to_dict("records"),
-    columnDefs=[{"field": i} for i in df.columns],
+    columnDefs=[{"field": i,"cellDataType" : 'text'} for i in df.columns],
+    # columnDefs=[{"field":'BLOCK NO',"cellDataType" : 'text' }],
     defaultColDef={"filter": True, "sortable": True, "resizable": True},
     className="ag-theme-alpine-dark",
 )
 # dynamic chart for APO recovery
-
-
-
 layout = dbc.Container(
     [dbc.Row(dcc.Dropdown(df['COLOR NAME'].unique(),multi=True,value=df['COLOR NAME'].unique()[0:2],id='dropdown')),
         dbc.Row(
@@ -55,7 +54,11 @@ layout = dbc.Container(
     ]),
         
     
-        dbc.Row(dcc.Graph(figure={},id='graph1'),style={'margin-bottom': '20px'}),
+        dbc.Row(dcc.Graph(figure={},id='graph1'),style={
+                # "width": "2000px",  # Set a large width for the graph container
+                "overflow-x": "auto",  # Enable horizontal scrolling
+                "white-space": "nowrap"  # Prevent graph from wrapping
+            }),
         grid
     ],
     fluid=True
@@ -69,9 +72,9 @@ layout = dbc.Container(
 )
 def update(dropdown_value):
     dff=df[df['COLOR NAME'].isin(dropdown_value)]
-    fig=px.histogram(dff,x=dff['APO NO'],y=dff['TOTAL  RECOVERY CBM FOR BLOCK']).update_layout(template="plotly_dark",xaxis=dict(type='category'))
-    rec=int(sum(dff['CUTTING QTY'])/sum(dff['CBM']))
-    dispactchqty=dff['DISPATCHED QTY'].sum()
-    instockqty=dff['CUTTING QTY'].sum()-dispactchqty
-    print(rec)
+    fig=px.histogram(dff,x=dff['BLOCK NO'],y=dff['Recovery']).update_layout(template="plotly_dark",xaxis=dict(type='category'))
+    rec=round(sum(dff['DISPATCHED QTY']+dff['SFT FOR BAL SLABS'])/sum(dff['CBM']),2)
+    
+    dispactchqty=round(dff['DISPATCHED QTY'].sum(),2)
+    instockqty=round(dff['CUTTING QTY'].sum()-dispactchqty,2)
     return fig,rec,dispactchqty,instockqty
